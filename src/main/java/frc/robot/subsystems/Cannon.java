89423598cdcd340c7db359f697f4d70c91b82e52
Sam <58117545+Sam-Malta@users.noreply.github.com>
2024-10-07 13:41:56 -0400

    Feat: logging and debugging things

--- src/main/java/frc/robot/subsystems/Cannon.java
+++ src/main/java/frc/robot/subsystems/Cannon.java
@@ -5,7 +5,15 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PnuematicsConstants;
+import edu.wpi.first.util.datalog.DataLog;
 import edu.wpi.first.wpilibj.Compressor;
+
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.util.datalog.BooleanLogEntry;
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+import edu.wpi.first.util.datalog.StringLogEntry;
+
+
 
 public class Cannon extends SubsystemBase{
 
@@ -18,12 +26,27 @@ public class Cannon extends SubsystemBase{
     private Solenoid power1;
     private Solenoid power2;
 
+    // Logging
+    private DataLog log;
+    private BooleanLogEntry compressorLog;
+    private DoubleLogEntry psiLog;
+    private StringLogEntry barrelLog;
+
+    // Barrel Status
+    private boolean leftSolenoidStatus;
+    private boolean rightSolenoidStatus;
+
     @Override
     public void periodic() {
         SmartDashboard.putBoolean("Compressor", compressor.isEnabled());
-        SmartDashboard.putNumber("PSI", compressor.getPressure());
+        SmartDashboard.putBoolean("Software Compressor Order", compressor.getPressureSwitchValue());
+        SmartDashboard.putNumber("Tank PSI", compressor.getPressure());
 
-       
+        SmartDashboard.putBoolean("Left Barrel Software Command", leftSolenoidStatus);
+        SmartDashboard.putBoolean("Right Barrel Software Command", rightSolenoidStatus);
+
+        SmartDashboard.putBoolean("Left Barrel Solenoid Status", this.sol4.get()); 
+        SmartDashboard.putBoolean("Right Barrel Solenoid Status", this.sol3.get());
     }
 
     public Cannon() {
@@ -34,22 +57,38 @@ public class Cannon extends SubsystemBase{
         this.power1 = new Solenoid(PneumaticsModuleType.REVPH, 1); // enables relay
         this.power2 = new Solenoid(PneumaticsModuleType.REVPH, 2); // enables board
         compressor = new Compressor(PneumaticsModuleType.REVPH);
+
+        // Data Logging
+        DataLogManager.start();
+
+        this.log = DataLogManager.getLog();
+        compressorLog = new BooleanLogEntry(log, "/cannonLogs/compressorLog");
+        psiLog = new DoubleLogEntry(log, "/cannonLogs/psiLog");
+        barrelLog = new StringLogEntry(log, "/cannonLogs/barrelLog");
+
+        // Barrel Status
+        this.leftSolenoidStatus = false;
+        this.rightSolenoidStatus = false;
     }
 
     public void rightPrimerOff(){ // Resets barrel primer after a solenoid is fired
         this.power0.set(false);
+        barrelLog.append("Right Barrel Primer: UNARMED");
     }
 
     public void rightPrimerOn(){ // Resets barrel primer after a solenoid is fired
         this.power0.set(true);
+        barrelLog.append("Right Barrel Primer: ARMED");
     }
 
     public void leftPrimerOff(){
         this.power1.set(false);
+        barrelLog.append("Left Barrel Primer: UNARMED");
     }
 
     public void leftPrimerOn(){
         this.power1.set(true);
+        barrelLog.append("Left Barrel Primer: ARMED");
     }
 
     public void startPnuematics(){
@@ -57,14 +96,22 @@ public class Cannon extends SubsystemBase{
         this.power0.set(true);
         this.power1.set(true);
         this.power2.set(true);
+        barrelLog.append("Pneumatic System Panel: Enabled");
+        barrelLog.append("Pneumatic System Armed: All Barrels Primers Armed");
     }
 
     public void left() {
         this.sol4.toggle(); // barrel 1
+        this.leftSolenoidStatus = !this.leftSolenoidStatus;
     }
     
     public void right() {
         this.sol3.toggle(); // barrel 2
+        this.rightSolenoidStatus = !this.rightSolenoidStatus;
     }
 
+    public void insanityCheck()
+    {
+        
+    }
 }
